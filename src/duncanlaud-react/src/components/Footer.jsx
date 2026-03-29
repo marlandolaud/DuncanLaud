bd3fe870bd18abb3f8d71caca1ee706ec9820879
mlaud <mlaud@outlook.com>
2026-03-29 10:47:10 -0400

    removed link to store from footer

--- src/duncanlaud-react/src/components/Footer.jsx
+++ src/duncanlaud-react/src/components/Footer.jsx
@@ -9,7 +9,6 @@ export default function Footer() {
         <div className="footer__links">
           <Link to="/" className="footer__link">Home</Link>
           <Link to="/about" className="footer__link">About</Link>
-          <a href="https://penthumb.com" className="footer__link" target="_blank" rel="noopener noreferrer">Store</a>
           <a href="mailto:christine@duncanlaud.com" className="footer__link">Contact</a>
           <Link to="/mygroup" className="footer__link">Birthday Groups</Link>
         </div>
